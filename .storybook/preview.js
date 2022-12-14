1983dd54549e07048d959c5e3466bc7ff11b44ee
Simon <simon.hoetten@aboutsource.net>
2022-12-14 17:28:24 +0100

    add addon-themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  themes: {
+    default: 'theme1',
+    target: 'root',
+    list: [
+      { name: 'theme1', class: 'theme1', color: '#f00' },
+      { name: 'theme2', class: 'theme2', color: '#0f0' },
+    ],
+  },
 }
